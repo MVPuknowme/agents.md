9f699ad8ddcbd216a96342419e4508af6c284d25
Mvpuknowme or Michael Vincent Patrick <130760115+MVPuknowme@users.noreply.github.com>
2026-09-02 17:04:15 -0700

    Update device-link.js

--- api/stripe/device-link.js
+++ api/stripe/device-link.js
@@ -2,7 +2,7 @@ module.exports = function handler(_req, res) {
   res.status(200).json({
     stripeDeviceLink: 'placeholder',
     activation: 'disabled',
-    liveCharge: false,
+    liveCharge: true,
     secretKeysExposed: false,
   });
 };
